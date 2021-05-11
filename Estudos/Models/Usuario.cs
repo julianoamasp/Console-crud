f104ec96718a4095a141e2aa6f07fce57a2fe676
Juliano da Silva Mendes <julianoamasp@gmail.com>
2021-05-10 22:51:37 -0300

    Criação de modelo

---
+++ Estudos/Models/Usuario.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudos.Models
+{
+    class Usuario
+    {
+        private int id;
+        private string nome;
+        private string email;
+        private string senha;
+
+        public Usuario()
+        {
+        }
+        public Usuario(int id, string nome, string email, string senha)
+        {
+            this.id = id;
+            this.nome = nome;
+            this.email = email;
+            this.senha = senha;
+        }
+        public int getId()
+        {
+            return this.id;
+        }
+        public string getNome()
+        {
+            return this.nome;
+        }
+        public string getEmail()
+        {
+            return this.email;
+        }
+        public string getSenha()
+        {
+            return this.senha;
+        }
+
+    }
+}
--- Estudos/Program.cs
+++ Estudos/Program.cs
@@ -1,6 +1,7 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
+using Estudos.Models;
 
 namespace Estudos
 {
@@ -222,7 +223,6 @@ namespace Estudos
 
                 string query = "SELECT * FROM administradores";
                 MySqlCommand comando = new MySqlCommand(query, conn);
-
                 conn.Open();
 
                 MySqlDataReader rd = comando.ExecuteReader();
@@ -242,41 +242,5 @@ namespace Estudos
             catch (Exception ex) { Console.WriteLine(ex.ToString()); }
             return usuarios;
         }
-    }
-
-    class Usuario
-    {
-        private int id;
-        private string nome;
-        private string email;
-        private string senha;
-
-        public Usuario()
-        {
-        }
-        public Usuario(int id, string nome, string email, string senha)
-        {
-            this.id = id;
-            this.nome = nome;
-            this.email = email;
-            this.senha = senha;
-        }
-        public int getId()
-        {
-            return this.id;
-        }
-        public string getNome()
-        {
-            return this.nome;
-        }
-        public string getEmail()
-        {
-            return this.email;
-        }
-        public string getSenha()
-        {
-            return this.senha;
-        }
-
     }
 }
