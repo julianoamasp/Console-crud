dad1f245a8611916cead64994cbd5a1039e0c2f6
Juliano da Silva Mendes <julianoamasp@gmail.com>
2021-05-11 08:03:27 -0300

    Inserção do Data Access Object

---
+++ Estudos/DAO/UsuarioDAO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Estudos.Models;
+using MySql.Data.MySqlClient;
+
+namespace Estudos.DAO
+{
+    class UsuarioDAO : Usuario
+    {
+        public MySqlCommand conn;
+    }
+}
